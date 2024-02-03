a2c1fbd29d545e1196d7fb24911bcad5396d6616
Jeremiah Korreck <korreckj328@gmailcom>
2024-02-03 15:33:03 -0500

    changed the information to be displayed in a sheet.  Needs some formating and thought.

---
+++ UDZPJ/InformationView.swift
@@ -0,0 +1,21 @@
+//
+//  InformationView.swift
+//  UDZPJ
+//
+//  Created by Jeremiah Korreck on 2/3/24.
+//
+
+import SwiftUI
+
+struct InformationView: View {
+    var body: some View {
+        Text("Written By: Jeremiah Korreck")
+        Text("Animal facts written by chatGPT 3.5")
+        
+        Text("This app is an experiment in getting a working image classifier to correctly label all the animals on the map of Detroit Zoo.")
+    }
+}
+
+#Preview {
+    InformationView()
+}
