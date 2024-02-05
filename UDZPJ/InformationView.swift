e35fd1c56c0cd14a8dcdcb0cddb410e765f940c6
Jeremiah Korreck <korreckj328@gmailcom>
2024-02-05 09:00:34 -0500

    formatting changes

--- UDZPJ/InformationView.swift
+++ UDZPJ/InformationView.swift
@@ -9,11 +9,13 @@ import SwiftUI
 
 struct InformationView: View {
     var body: some View {
-        Spacer()
         Text("Unofficial Detroit Zoo Photo Journal\n")
             .bold()
+            .padding(20)
+            .underline()
         Text("Written By: Jeremiah Korreck\n\n")
         Text("Animal facts written by chatGPT 3.5\n")
+            .padding(10)
         Text("This app is an experiment in getting a working image classifier to correctly label all the animals on the map of Detroit Zoo.")
             .padding(10)
         Spacer()
